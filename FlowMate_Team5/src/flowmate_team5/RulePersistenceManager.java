9e6266eb2929339db3f0e22969a9f4b210db9b03
asiddiqa-1 <a.siddiqa1@studenti.unisa.it>
2025-12-14 00:15:17 +0100

    Added loadRules() to RulePersistenceManager

--- FlowMate_Team5/src/flowmate_team5/RulePersistenceManager.java
+++ FlowMate_Team5/src/flowmate_team5/RulePersistenceManager.java
@@ -1,6 +1,7 @@
 package flowmate_team5;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RulePersistenceManager {
@@ -21,5 +22,31 @@ public class RulePersistenceManager {
             System.err.println("[Persistence] Error saving rules.");
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Load all rules from file
+     */
+    public static List<Rule> loadRules() {
+        List<Rule> rules = new ArrayList<>();
+        File file = new File(FILE_NAME);
+
+        if (!file.exists()) {
+            System.out.println("[Persistence] No saved rules found. Starting fresh.");
+            return rules;
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
+            Object obj = ois.readObject();
+            if (obj instanceof List<?>) {
+                rules = (List<Rule>) obj;
+            }
+            System.out.println("[Persistence] Loaded " + rules.size() + " rules.");
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("[Persistence] Error loading rules.");
+            e.printStackTrace();
+        }
+
+        return rules;
     }
 }
