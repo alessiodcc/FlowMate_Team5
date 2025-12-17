4532a68e7e5258a884457438b97a6e832cae60e5
ester <e.sabatino14@studenti.unisa.it>
2025-12-17 17:54:52 +0100

    Implementing the DayOfTheMonthTrigger creator

---
+++ FlowMate_Team5/src/flowmate_team5/factory/creators/DayOfTheMonthTriggerCreator.java
@@ -0,0 +1,13 @@
+package flowmate_team5.factory.creators;
+
+import flowmate_team5.factory.CreatorTrigger;
+import flowmate_team5.models.Trigger;
+import flowmate_team5.models.triggers.DayOfTheMonthTrigger;
+
+public class DayOfTheMonthTriggerCreator implements CreatorTrigger {
+
+    @Override
+    public Trigger createTrigger() {
+        return new DayOfTheMonthTrigger();
+    }
+}
