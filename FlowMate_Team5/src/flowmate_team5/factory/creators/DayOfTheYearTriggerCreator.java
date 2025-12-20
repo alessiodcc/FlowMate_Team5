bded356307380795fb46c873d511f6a09352e8cc
Sara <s.pecoraro19@studenti.unisa.it>
2025-12-20 12:56:06 +0100

    Update of the day of the year

--- FlowMate_Team5/src/flowmate_team5/factory/creators/DayOfTheYearTriggerCreator.java
+++ FlowMate_Team5/src/flowmate_team5/factory/creators/DayOfTheYearTriggerCreator.java
@@ -7,18 +7,14 @@ import java.time.Month;
 
 public class DayOfTheYearTriggerCreator implements CreatorTrigger {
 
-    /**
-     * This method is required by the CreatorTrigger interface.
-     * It creates a default instance (e.g., January 1st) to satisfy the compiler.
-     */
     @Override
     public Trigger createTrigger() {
-        return new DayOfTheYearTrigger(Month.JANUARY, 1);
+        // Returns a default instance (January 1st)
+        return new DayOfTheYearTrigger();
     }
 
     /**
-     * Specific method to create the trigger with user data.
-     * Used by the Controller.
+     * Helper to create with specific values.
      */
     public Trigger createTrigger(Month month, int day) {
         return new DayOfTheYearTrigger(month, day);
