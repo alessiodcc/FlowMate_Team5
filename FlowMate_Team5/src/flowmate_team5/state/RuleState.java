725669dde1014c22a5a170d2345689f277de93ac
Sara <s.pecoraro19@studenti.unisa.it>
2025-12-21 17:34:53 +0100

    Update of the comments of the RuleState

--- FlowMate_Team5/src/flowmate_team5/state/RuleState.java
+++ FlowMate_Team5/src/flowmate_team5/state/RuleState.java
@@ -4,7 +4,12 @@ import flowmate_team5.core.Rule;
 
 import java.io.Serializable;
 
+/* Defines the contract for the various states a Rule can inhabit, supporting persistence. */
 public interface RuleState extends Serializable {
+
+    /* Executes the logic specific to the current state, using the provided Rule context. */
     void check(Rule context);
+
+    /* Returns the operational status of the state (true for active, false otherwise). */
     boolean isActive();
 }
