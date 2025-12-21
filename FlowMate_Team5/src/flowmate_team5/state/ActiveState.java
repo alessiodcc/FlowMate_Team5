5cee4cc472cdd1be588281435f29f5f86297da13
alessiodcc <a.dellacorte23@studenti.unisa.it>
2025-12-21 15:55:01 +0100

    Fixing the sleeping logic

--- FlowMate_Team5/src/flowmate_team5/state/ActiveState.java
+++ FlowMate_Team5/src/flowmate_team5/state/ActiveState.java
@@ -6,25 +6,20 @@ public class ActiveState implements RuleState {
 
     @Override
     public void check(Rule context) {
-
         if (context.getTrigger() != null && context.getTrigger().isTriggered()) {
 
             context.execute();
-            System.out.println("[Rule Fired]: " + context.getName());
 
             long sleepDuration = context.getSleepDurationMillis();
 
             if (context.isRepeatable()) {
-
                 if (sleepDuration > 0) {
                     long wakeUpTime = System.currentTimeMillis() + sleepDuration;
                     context.setState(new CooldownState(wakeUpTime));
                 }
-
             } else {
                 context.setState(new InactiveState());
             }
-
         }
     }
 
